306a77adff80418d14611308fde4a7fee2b8f157
Philosophers Fuel <donsells3535@gmail.com>
2023-09-04 11:51:41 -0700

    intergers add sub multi div expo

--- intergers.py
+++ intergers.py
@@ -1,7 +1,19 @@
 #intergers can be positive numbers
-print ('1')
+# print ('1')
 #intergers can be negative numbers
-print ('-1')
+# print ('-1')
 #intergers can be zero
-print ('0')
+# print ('0')
+
+# arithmetic operators
+#addition
+print (2+3)
+#subtraction
+print (2-3)
+#multiplication
+print (3*4)
+#division
+print (4/2)
+#exponets
+print (5**2)
 
